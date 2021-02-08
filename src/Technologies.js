f71285d693c876195140ca3564800959bd49892b
Alex <example#gmail.com>
2021-02-08 09:16:16 +0300

    rect first

---
+++ src/Technologies.js
@@ -0,0 +1,15 @@
+function Technologies(){
+    return (
+      <div className="App">
+        Simple HTML
+        <ul>
+          <li>css</li>
+          <li>html</li>
+          <li>js</li>
+          <li>react</li>
+        </ul>
+      </div>
+    )
+  }
+
+  export default Technologies;
